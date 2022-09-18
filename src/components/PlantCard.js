9bc639abc3e2e7feca7ea44ece483a248cb51a55
Jonathan <jonathanmoorevla@gmail.com>
2022-09-17 21:28:05 -0500

    Add InStock/SoldOut functionality

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function PlantCard({plant}) {
   const {id, name, image, price} = plant
+  const [inStock, setInStock] = useState(true)
   
+  function handleInStock() {
+    setInStock(!inStock)
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleInStock}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleInStock}>Out of Stock</button>
       )}
     </li>
   );
